cfb34fa27d382a514f802dfb9a1c522c0607d123
Aarav Porwal <aarav.25bcs10146@sst.scaler.com>
2026-06-15 11:30:53 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,69 @@
 #include <bits/stdc++.h>
+#include <unistd.h>
+
 using namespace std;
+
+void typeCommand(const string& input,
+                 const vector<string>& builtins) {
+    string cmd = input.substr(5);
+
+    // Check builtins
+    for (const string& builtin : builtins) {
+        if (builtin == cmd) {
+            cout << cmd << " is a shell builtin" << '\n';
+            return;
+        }
+    }
+
+    // Check PATH
+    char* path_env = getenv("PATH");
+    if (path_env == nullptr) {
+        cout << cmd << ": not found" << '\n';
+        return;
+    }
+
+    string path(path_env);
+    istringstream ss(path);
+
+    string directory;
+    while (getline(ss, directory, ':')) {
+        string full_path = directory + "/" + cmd;
+
+        if (access(full_path.c_str(), X_OK) == 0) {
+            cout << cmd << " is " << full_path << '\n';
+            return;
+        }
+    }
+
+    cout << cmd << ": not found" << '\n';
+}
+
 int main() {
-  // Flush after every std::cout / std:cerr
-  cout << std::unitbuf;
-  cerr << std::unitbuf;
+    cout << unitbuf;
+    cerr << unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  while(true){
+    vector<string> builtins = {"exit", "echo", "type"};
 
-    cout << "$ ";
-    string input;
-    getline(cin, input);
+    while (true) {
+        cout << "$ ";
 
+        string input;
+        if (!getline(cin, input))
+            break;
 
-        if(input.substr(0, 5) == "type "){
-          if(input.substr(5,4) == "echo" || input.substr(5,4) == "exit" || input.substr(5,4) == "type"){
-             cout<<input.substr(5,4)<<" "<<"is a shell builtin"<<"\n";
-          }else{
-            cout<<input.substr(5)<< ": not found"<<"\n";
-          }
-        }else if(input == "exit") break;
-        else if (input.substr(0, 5) == "echo ") {
-        cout << input.substr(5) <<endl;
+        if (input == "exit") {
+            break;
+        }
+        else if (input.rfind("echo ", 0) == 0) {
+            cout << input.substr(5) << '\n';
+        }
+        else if (input.rfind("type ", 0) == 0) {
+            typeCommand(input, builtins);
         }
         else {
-        cout << input << ": command not found" <<endl;
+            cout << input << ": command not found" << '\n';
         }
+    }
 
-  }
-
-
-  }
-
+    return 0;
+}
